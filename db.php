d990b4d0b2ab63810298ae6fde7d6553305ca31a
yumnaankyprita <yumnaankyprita@gmail.com>
2024-12-19 11:33:51 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.80.1.67:3308;dbname=db_eskrim',
+    'username' => 'Prita',
+    'password' => '180206',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
